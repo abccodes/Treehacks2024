af3d05052aa290ad71fd5d7a2f8be4ca99b77b0d
SirKentut <pierreharbin@gmail.com>
2024-02-17 15:42:47 -0800

    landing page hero

--- treehacks2024/src/App.tsx
+++ treehacks2024/src/App.tsx
@@ -19,7 +19,9 @@ export default function App() {
   };
 
     return (
-      <main className="container max-w-2xl flex flex-col gap-8">
+      // <main className="container max-w-2xl flex flex-col gap-8">
+      <main className="container max-w-full flex flex-col gap-8">
+
 
         <ShuffleHero />
       <Authenticated>
--- treehacks2024/src/Shufflehero.jsx
+++ treehacks2024/src/Shufflehero.jsx
@@ -3,7 +3,10 @@ import { useEffect, useRef, useState } from "react";
 
 const ShuffleHero = () => {
   return (
-    <section className="w-full px-8 py-12 grid grid-cols-1 md:grid-cols-2 items-center gap-8 max-w-6xl mx-auto">
+    // <section className="w-full px-8 py-12 grid grid-cols-1 md:grid-cols-2 items-center gap-8 max-w-6xl mx-auto">
+    // <section className="w-full px-8 py-12 grid grid-cols-1 md:grid-cols-[1fr_2fr] items-center gap-8 mx-auto">
+    <section className="w-full px-8 py-12 grid grid-cols-1 md:grid-cols-[1fr_2fr] items-center gap-8 max-w-6xl mx-auto">
+
       <div>
         <span className="block mb-4 text-xs md:text-sm text-indigo-500 font-medium">
           Making diagnosis easy
