388b08b6523acf5a5d52fe6c67832bd3c11bbfd0
drkchn <57195399+drkchn@users.noreply.github.com>
2024-02-17 20:16:40 -0800

    Removed file upload button

--- treehacks2024/src/main/pages/InputPage.tsx
+++ treehacks2024/src/main/pages/InputPage.tsx
@@ -1,14 +1,5 @@
-import React, { useState } from "react";
-import Container from "../components/Upload.tsx";
-import { UploadButton, UploadFileResponse } from "@xixixao/uploadstuff/react";
-import {
-  //   Authenticated,
-  //   Unauthenticated,
-  //   useConvexAuth,
-  useMutation,
-  //   useQuery,
-} from "convex/react";
-import { api } from "../../../convex/_generated/api";
+import React from "react";
+import ContainerWithUpload from "../components/Upload.tsx";
 import "@xixixao/uploadstuff/react/styles.css";
 import { Card, CardContent } from "@/components/ui/card";
 import {
@@ -22,45 +13,11 @@ import Autoplay from "embla-carousel-autoplay";
 
 const InputPage: React.FC = () => {
 
-  const [uploadUrl, setUploadUrl] = useState(''); // for getting the URLs
-
-  const generateUploadUrl = useMutation(api.files.generateUploadUrl);
-  const saveStorageId = useMutation(api.files.saveStorageId);
-  const saveAfterUpload = async (uploaded: UploadFileResponse[]) => {
-
-    // Generate a new upload URL for displaying
-    const newUploadUrl = await generateUploadUrl();
-    setUploadUrl(newUploadUrl);
-
-    await saveStorageId({
-      uploaded: { storageId: (uploaded[0].response as any).storageId },
-    });
-  };
-
   return (
     <div>
       <div className="flex justify-center m-10">
         <div className="flex-column">
-          <Container />
-
-          // Display the upload URL if it exists
-          {uploadUrl && (
-          <p>Upload URL: <a href={uploadUrl}>{uploadUrl}</a></p>
-        )}
-
-          <UploadButton
-
-            // Generate and set upload URL when button is used
-            uploadUrl={() => generateUploadUrl().then(url => { setUploadUrl(url); return url; })}
-
-            
-            fileTypes={[".pdf", "image/*"]}
-            onUploadComplete={saveAfterUpload}
-            onUploadError={(error: unknown) => {
-              // Do something with the error.
-              alert(`ERROR! ${error}`);
-            }}
-          />{" "}
+          <ContainerWithUpload />
         </div>
       </div>
       <div className="flex justify-center ">
