b67def4cf2882bb6a326d62747dc9f5be2dedb08
Tommy Yau <wwkb1233@gmail.com>
2024-02-17 17:38:44 -0800

    remove unuse

--- treehacks2024/src/App.tsx
+++ treehacks2024/src/App.tsx
@@ -1,22 +1,11 @@
-import { Button } from "@/components/ui/button";
-import { SignInButton, UserButton } from "@clerk/clerk-react";
-import {
-  Authenticated,
-  Unauthenticated,
-  useConvexAuth,
-  useMutation,
-  useQuery,
-} from "convex/react";
-import { UploadButton, UploadFileResponse } from "@xixixao/uploadstuff/react";
+import { Authenticated, Unauthenticated, useConvexAuth } from "convex/react";
 import "@xixixao/uploadstuff/react/styles.css";
-import { api } from "../convex/_generated/api";
 import Test from "./components/test";
 import ShuffleHero from "./Shufflehero.jsx";
 import Navbar from "./main/components/Navbar.tsx";
 import InputPage from "./main/pages/InputPage.tsx";
 
 export default function App() {
-
   console.log(useConvexAuth());
 
   return (
